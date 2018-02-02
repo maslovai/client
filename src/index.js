b533ce1ce4a2f45471564fbea12759de40d7ff5d
Pmookerjee <pmookerjee@gmail.com>
2018-02-01 19:56:31 -0800

    please work

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ let app = express();
 // app.use(morgan('common'));
 
 app.use(express.static(`${__dirname}/build`));
-app.use('*', (req, res) => res.sendFile(`${__dirname}/build/index.html`));
+app.use('*', (req, res) => res.send(`${__dirname}/build/index.html`));
 
 app.listen(process.env.PORT, () => {
   console.log('Server up on port ', process.env.PORT);
