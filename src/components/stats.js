ae9d20b38eeee0cc74c82f3329b46ad28b98092d
Cole Allen <colesss>
2018-02-08 19:04:21 -0800

    added preliminary stats components

--- src/components/stats.js
+++ src/components/stats.js
@@ -1,56 +1,46 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import ReactDOM from 'react-dom';//is react-redux doing this?
+import Chart from 'react-d3-core';//need to install
+import LineChart from 'react-d3-basic';//need to install
+import * as userAction from '../app/actions/users';//we need to create a user action
+
+class UserStats extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = this.props.userStats || {};//what kind of state do we need
+    }
+
+    componentWillMount(){
+        //not sure what we need to use this for, if we need to use it
+    }
+    
+    render() {
+        return (
+            <div>
+                <div>
+                    <h2>Completed Tasks, By Users </h2>
+
+                </div>
+            </div>
+        )
+    }
+}
+
+
+const mapStateToProps = (state) => ({
+    userStats: state.userStats
+  });
+  
+const mapDispatchToProps = (dispatch, getState) => ({
+    updateProfile: (user) => dispatch(profileActions.update(user))
+});//do we need this for charts and stats?
+  
+export default connect(mapStateToProps, mapDispatchToProps)(UserStats);
+
+
+
 //demo code from FusionCharts (a react chart plugin)
 //https://www.fusioncharts.com/react-charts/#/demos/ex2
-
-FusionCharts.ready(function () {
-  var myDataSource = {
-      chart: {
-          caption: "Age profile of website visitors",
-          subcaption: "Last Year",
-          startingangle: "120",
-          showlabels: "0",
-          showlegend: "1",
-          enablemultislicing: "0",
-          slicingdistance: "15",
-          showpercentvalues: "1",
-          showpercentintooltip: "0",
-          plottooltext: "Age group : $label Total visit : $datavalue",
-          theme: "ocean"
-      },
-      data: [
-          {
-              label: "Teenage",
-              value: "1250400"
-          },
-          {
-              label: "Adult",
-              value: "1463300"
-          },
-          {
-              label: "Mid-age",
-              value: "1050700"
-          },
-          {
-              label: "Senior",
-              value: "491000"
-          }
-      ]
-  }
-  var props_pie_chart = {
-      id: "pie_chart",
-      type: "pie3d",
-      width: "80%",
-      height: 400,
-      dataFormat: "json",
-      dataSource: myDataSource
-  };
-  ReactDOM.render(
-      <ReactFC {...props_pie_chart} />,
-      document.getElementById('chart-container')
-  );
-})
-
-<div id="chart-container"></div>
-
 //or
 //https://medium.com/localmed-engineering/svg-pie-chart-using-react-and-d3-43a381ce7246
-
